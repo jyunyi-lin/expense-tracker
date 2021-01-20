d38a3fc844328b8244dd9ac8648d69d2a9c3c24f
chuni-lin <william811566@gmail.com>
2021-01-20 09:29:02 +0800

    feat: update mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@ const db = mongoose.connection
 
 // 連線資料庫
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-db.on('error', () => console.error('MongoDB error 0_0'))
+db.on('error', () => console.log('MongoDB error!'))
 db.once('open', () => console.log('MongoDB connected!'))
 
 module.exports = db
